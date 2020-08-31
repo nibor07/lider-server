8b263944e958f5053924ed3546edec5aad93fda9
Robinson Elias Maqui Canaviri <rmaqui.trebolit@bancochile.cl>
2020-08-31 15:52:17 -0400

    Se cambia para CORS

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 require('dotenv').config();
 
+//Configuracion BD
 const conectarDB = require('./config/db');
+
+//Para CORS
 const cors = require('cors');
 
 // crear el servidor
 const app = express();
 
-//Conectar a la base de datos
-conectarDB();
+const port = process.env.SERVER_PORT || 4000;
 
 //Habilitar Cors
 app.use(cors());
@@ -19,15 +21,17 @@ app.use( express.static('public') );
 // Lectura y parseo del body
 app.use( express.json() );
 
-const port = process.env.port || 4000;
-
 app.use('/api/buscar', require('./routes/auths'));
+
+//Conectar a la base de datos
+conectarDB();
 
 // Definir la página main
 app.get('/', (req, res) =>{
     res.send('Hola Nibor')
 });
 // arrancar la app
-app.listen( port, '0.0.0.0', () => {
+//app.listen( port, '0.0.0.0', () => {
+app.listen( port, () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 })
